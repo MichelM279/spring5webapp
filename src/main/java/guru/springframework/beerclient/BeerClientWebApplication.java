2f6bcb2140e13a9d8f698cad32f94a96167d44cb
Michel Mangold <mangold@bitside.de>
2024-07-05 11:50:56 +0200

    Assignment 2: init client project

---
+++ src/main/java/guru/springframework/beerclient/BeerClientWebApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.beerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerClientWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerClientWebApplication.class, args);
+	}
+
+}
